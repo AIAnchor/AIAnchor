400585e9df4c778df47848c6fc171c0f380dd3e1
YOUNGGYUJUNG <wjddudrb190@naver.com>
2025-08-05 13:47:09 +0900

    Add Project

---
+++ aianchor/aianchor/src/test/java/com/boradcast/aianchor/AianchorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boradcast.aianchor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AianchorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
