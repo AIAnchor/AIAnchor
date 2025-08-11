cba7e31e9721592d8e08ad40035df69c73e95604
kks1177 <kks1177.dev@gmail.com>
2025-08-11 16:57:18 +0900

    Initial AIAnchor Settings

--- aianchor/aianchor/src/main/java/com/boradcast/aianchor/AianchorApplication.java
+++ aianchor/aianchor/src/main/java/com/boradcast/aianchor/AianchorApplication.java
@@ -8,7 +8,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class AianchorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AianchorApplication.class, args);
+		try {
+			SpringApplication.run(AianchorApplication.class, args);
+		} catch (Exception e) {
+		    System.err.println("\n\n !!!예외 발생: " + e.getMessage());
+		    e.printStackTrace();
+		}
 	}
-
 }
